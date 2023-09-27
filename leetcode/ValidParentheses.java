2629c128a86f0a8262ba6f8d0ae0b4184f45ddd9
Wesley Crowe <wesleycrowe999@gmail.com>
2023-09-27 16:00:21 -0700

    Valid Parentheses solution

---
+++ leetcode/ValidParentheses.java
@@ -0,0 +1,39 @@
+class ValidParentheses {
+    public boolean isValid(String s) {
+
+        //create a stack
+        Stack<Character> parens = new Stack<>();
+
+        for (int i=0; i<s.length(); i++){
+            
+            //get the current character
+            char currChar = s.charAt(i);
+
+            //if the current character is an opening bracket, put into stack
+            if (currChar == '(' || currChar == '[' || currChar == '{'){
+                parens.push(currChar);
+            }
+            //if the current character is a closing bracket, pop from the stack
+            else{
+                //if the stack is already empty, then not valid
+                if (parens.isEmpty()){ return false; }
+
+                //if the closing bracket matches, pop the stack
+                char top = parens.peek();
+                if ( (top == '(' && currChar == ')') ||
+                     (top == '[' && currChar == ']') ||
+                     (top == '{' && currChar == '}')
+                    ){
+                        parens.pop();
+                }
+                
+                //if it does not match, then not valid
+                else{
+                    return false;
+                }
+            }
+        }
+        //if the stack is empty, then valid
+        return parens.isEmpty();
+    }
+}
